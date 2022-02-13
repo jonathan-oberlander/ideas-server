2030652908b726e099d6aa737da9c183508f891b
jonathan-oberlander <jonathan.oberlander@smartpension.co.uk>
2022-02-13 15:44:21 +0000

    created basic swagger integration

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,24 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 const port = process.env.PORT;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Ideas')
+    .setDescription(
+      'A place to see, post, vote and comment on ideas in the style of twitter or reddit.',
+    )
+    .setVersion('0.0.1')
+    .addTag('ideas')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('documentation', app, document);
+
   await app.listen(port);
   Logger.log(`Server running on port ${port}`, 'Bootstrap');
 }
